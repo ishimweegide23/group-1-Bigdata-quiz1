615b02df65dab863d0c741de5462acca3d6a7aa4
kwibuka <ishimweegide20@gmail.com>
2025-06-26 13:59:12 +0300

    Update age_calculator.py

--- age_calculator.py
+++ age_calculator.py
@@ -2,6 +2,21 @@ def calculate_age(birth_year):
     current_year = 2025
     return current_year - birth_year
 
+# Get user input
 name = input("Enter your name: ")
-year = int(input("Enter your birth year (e.g., 2000): "))
-print(f"Hi {name}, your age is {calculate_age(year)}")
+
+while True:
+    try:
+        year_of_birth = int(input("Enter your year of birth (e.g., 2000): "))
+        if 1900 <= year_of_birth <= 2025:
+            break
+        else:
+            print("Please enter a valid year between 1900 and 2025.")
+    except ValueError:
+        print("Invalid input. Please enter a numeric year.")
+
+# Calculate age
+age = calculate_age(year_of_birth)
+
+# Display result
+print(f"Thank you\n{name}, your age is: {age}")
